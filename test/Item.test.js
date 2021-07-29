8f6f2e68db3f8c1f424bc16e5fd589edcef4c67d
afreensafdar <afreen.leo502@gmail.com>
2021-07-29 09:38:17 -0500

    datatype of test edit

--- test/Item.test.js
+++ test/Item.test.js
@@ -1,4 +1,3 @@
-// const Item= require('../models/Item');
 const {sequelize} = require('../db')
 const { Item } = require('../models/Item')
 
@@ -8,35 +7,34 @@ describe('Item Object',() => {
     beforeAll(async () => {
         await sequelize.sync({force: true})
     })
-
-    // const baseball= new Item('baseball','ball.jpeg','sports',5.99,
-    // 'Leather cover with raised seams and Solid cork composition core')
-   // const racquets= new Item('racquets','light weight and good net',12.99,'Sports','racquets.png')
       //1-Data types test
         test ('Item has name', async ()=> {
-            const testItem = await Item.create({name: 'baseball', image: 'ball.jepeg', category: 'sports', price: 5.99, description: 'Leather cover with raised seams and Solid cork composition core' })
+            const testItem = await Item.create({name: 'baseball', image: 'ball.jepeg', category: 'home', price: 5.99, description: 'Leather cover with raised seams and Solid cork composition core' })
             expect(testItem.name).toBe('baseball');
+            });
+
+        test ('Item has image', async ()=> {
+                const testItem = await Item.create({name :'baseball',image: 'ball.jpeg'})
+                expect(testItem.image).toBe('ball.jpeg');
+                });
+    
+    
+        test ('Item has category', async ()=> {
+            const testItem = await Item.create({name:'baseball',image:'ball.jpeg',category: 'home',price: 5.99,})
+            expect(testItem.category).toBe('home');
             });
     
         test ('Item has price', async ()=> {
-            const testItem = await Item.create({price: 5.99})
-            expect(testItem.price).toBe(5.99);
-            });
-    
-        test ('Item has category', async ()=> {
-            const testItem = await Item.create({category: 'sports'})
-            expect(testItem.category).toBe('sports');
-            });
-    
-        test ('Item has image', async ()=> {
-            const testItem = await Item.create({image: 'ball.jpeg'})
-            expect(testItem.image).toBe('ball.jpeg');
-            });
-    
+                const testItem = await Item.create({name:'baseball',image:'ball.jpeg',price: 5.99})
+                expect(testItem.price).toBe(5.99);
+                });
+                
+
         test('name is of type string', async ()=> {
             const testItem = await Item.create({name: 'baseball'})
             expect(typeof testItem.name).toBe('string');
             })
+
     
         test('price is of type number', async () => {
             const testItem = await Item.create({price: 5.99})
@@ -44,4 +42,3 @@ describe('Item Object',() => {
             })
 
         })
-    
